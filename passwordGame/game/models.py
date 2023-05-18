b3834e55a2dcacdcf0c61a95626508855d3631b5
backanddy <anddyneves.dev@gmail.com>
2023-05-18 13:37:20 -0300

    CHORE(app): add model method

--- passwordGame/game/models.py
+++ passwordGame/game/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+import random
 # Create your models here.
+
+def StarGame():
+    gameStart = ''
+    if gameStart == True:
+        outline = True    
+    else: 
+        outline = False
+    
+        if outline == True:
+            button = "btn-outline-secondary"
+        else:
+            button = "btn-secondary"
+
+
+
+red = 'buttonRed'
+green = 'buttonGreen'
+blue = 'buttonBlue'
+yellow = 'buttonYellow'
+purple = 'buttonPurple'
+gameButtons = [red, green, blue, yellow, purple]
+
+def GeneratedButtons():
+    buttons = []
+    
+def RandomButtons():
+
+    # rand = random.shuffle(gameButtons)
+    rand = gameButtons[0]
+    
+    return rand
+
+def  Game(): 
+    click = ''
+    cont = []
+        
+    
+    def GameCount():
+        if click == RandomButtons:
+            cont.append(click)
+            GameCount()
+        else:
+            exit = ''
+                               
+                
--- passwordGame/game/views.py
+++ passwordGame/game/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.http import HttpResponse
+from . import models
 # Create your views here.
+def index(request):
+    template = loader.get_template('index.html')    
+    return HttpResponse(template.render())
